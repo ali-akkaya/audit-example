2c56478ba02e3baeed300ae19dc6b15c90992ceb
Ali AKKAYA <aliakkaya@havelsan.com.tr>
2024-10-30 13:35:44 +0300

    Add baseEntity and one sample entity

---
+++ src/main/java/me/aliakkaya/auditexample/entity/Person.java
@@ -0,0 +1,12 @@
+package me.aliakkaya.auditexample.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person extends BaseEntity{
+
+
+
+}
