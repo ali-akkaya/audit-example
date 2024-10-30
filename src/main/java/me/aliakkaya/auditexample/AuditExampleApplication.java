c1a8712ecc4e0c69ca7846f656ca9fbd4df228a5
Ali AKKAYA <aliakkaya@havelsan.com.tr>
2024-10-30 10:23:38 +0300

    Initial commit

---
+++ src/main/java/me/aliakkaya/auditexample/AuditExampleApplication.java
@@ -0,0 +1,13 @@
+package me.aliakkaya.auditexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditExampleApplication.class, args);
+	}
+
+}
