86ada4be0c872c3f1333e26cb25fd9be329c46dc
Ali AKKAYA <aliakkaya@havelsan.com.tr>
2024-10-30 14:39:57 +0300

    --MappedSuperClass for extending base entity. --EntityListener, CreatedDate, LastModifiedDate are class level annotations for auditing. --EnableJpaAuditing for enabling auditing it can be written in another configuration class as well. Without it auditing will not work.

--- src/main/java/me/aliakkaya/auditexample/AuditExampleApplication.java
+++ src/main/java/me/aliakkaya/auditexample/AuditExampleApplication.java
@@ -2,8 +2,10 @@ package me.aliakkaya.auditexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AuditExampleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/me/aliakkaya/auditexample/entity/BaseEntity.java
+++ src/main/java/me/aliakkaya/auditexample/entity/BaseEntity.java
@@ -1,13 +1,29 @@
 package me.aliakkaya.auditexample.entity;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
 }
